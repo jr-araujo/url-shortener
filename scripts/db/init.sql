24b9f235405d77ca88041f392bed3c7fc3efa8db
jaraujo <jaraujo@tillster.com>
2023-10-30 23:16:03 +0000

    fix: removed unused fields in db init table

--- scripts/db/init.sql
+++ scripts/db/init.sql
@@ -2,8 +2,5 @@ CREATE TABLE shorten_url (
     id SERIAL PRIMARY KEY,
     code VARCHAR(6) NOT NULL,
     original VARCHAR(1000) NOT NULL,
-    shorten_url VARCHAR(25) NOT NULL,
-    access_number INT NOT NULL,
-    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+    access_number INT NOT NULL
 );
